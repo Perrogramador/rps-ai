7e512d9a3198c53b6e1fb4372a0eb64d9d316e7b
Rocher Colás <rocher@mindfury.co.uk>
2018-06-20 19:05:52 +0100

    interface working

--- tagger.py
+++ tagger.py
@@ -12,12 +12,14 @@ class Tagger:
         self.cap = cv2.VideoCapture(0)
         self.tagIdx = 0
         self.count = 0
-        self.h = 0
-        self.s = 0
-        self.v = 0
-        self.H = 0
-        self.S = 0
-        self.V = 0
+        self.h = 99
+        self.s = 41
+        self.v = 156
+        self.H = 115
+        self.S = 141
+        self.V = 255
+
+        self.capture()
     
     def nextTag(self):
         self.tagIdx = (self.tagIdx + 1) % 3
@@ -54,10 +56,10 @@ class Tagger:
             frame = cv2.flip(frame, 1)
             frame = cv2.rectangle(frame, *self.FRAME_COORDS, (255,0,0), 2)
 
-            cropped = frame[
-                self.FRAME_COORDS[0][1] + 2 : self.FRAME_COORDS[1][1] - 1,
-                self.FRAME_COORDS[0][0] + 2 : self.FRAME_COORDS[1][0] - 1
-            ]
+            # cropped = frame[
+                # self.FRAME_COORDS[0][1] + 2 : self.FRAME_COORDS[1][1] - 1,
+                # self.FRAME_COORDS[0][0] + 2 : self.FRAME_COORDS[1][0] - 1
+            # ]
 
             # Convert frame to HSV
             hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
@@ -67,13 +69,13 @@ class Tagger:
 
 
             mask = cv2.inRange(hsv, lower_blue, upper_blue)
-            res = cv2.bitwise_and(hsv,frame, mask= mask)
+            res = cv2.bitwise_and(frame, frame, mask= mask)
 
             self.write(hsv)
             #cv2.imshow("frame", frame)
             cv2.imshow("hsv", hsv)
-            cv2.imshow("mask", mask)
-            #cv2.imshow("res", res)
+            # cv2.imshow("mask", mask)
+            cv2.imshow("res", res)
             cv2.createTrackbar("h", "hsv", 0, 255, self.set_h)
             cv2.createTrackbar("s", "hsv", 0, 255, self.set_s)
             cv2.createTrackbar("v", "hsv", 0, 255, self.set_v)
